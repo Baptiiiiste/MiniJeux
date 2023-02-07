dbeafe87c65b5e9c24242c19c453d388aa6ef101
clverdoire <Clement.verdoire@etu.uca.fr>
2023-02-07 15:27:25 +0100

    ajout navbar pour phone

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,42 @@ import styles from "@/styles/Header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser, faBars } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+
+	function showMenu() {
+		setOpen(!isOpen)
+	}
+
+	const [isOpen, setOpen] = useState(false);
+
+	useEffect(() => {
+		let x = document.getElementsByClassName(styles.menuHidden)[0];
+		x.style.display = isOpen ? "flex":"none"
+	}, [isOpen])
+
   return (
     <header className={styles.header}>
-		<label className={styles.navButton}>
-			<FontAwesomeIcon className={styles.navIcon} icon={faBars}/>
-		</label>
+
+		{}
+		<FontAwesomeIcon className={styles.navIcon} icon={faBars} onClick={showMenu}/>
 		<nav className={styles.links}>
-			<Link href="/allumettes">Allumettes </Link>
+			<Link href="/allumettes">Allumettes</Link>
 			<Link href="/blackjack">Blackjack</Link>
 			<Link href="/tictactoe">Tic Tac Toe</Link>
 			<Link href="/classement">Classement</Link>
 		</nav>
+
+		<div className={styles.menuHidden}>
+			<FontAwesomeIcon className={styles.navIconHidden} icon={faBars} onClick={showMenu}/>
+			<nav className={styles.linksHidden}>
+				<Link href="/allumettes">Allumettes</Link>
+				<Link href="/blackjack">Blackjack</Link>
+				<Link href="/tictactoe">Tic Tac Toe</Link>
+				<Link href="/classement">Classement</Link>
+			</nav>
+		</div>
 
 		<div className={styles.account}>
 			
