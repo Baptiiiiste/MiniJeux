3362b2aae53e5926967c60e052f5c7763aeeca46
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2023-02-21 11:13:47 +0100

    useFetch hook

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // API_LOGIN : "https://minijeux.nojii.fr/api/connexion", 
-    // API_SIGNIN : "https://minijeux.nojii.fr/api/inscription", 
-    API_LOGIN: "http://localhost:3000/api/connexion",
-    API_SIGNIN: "http://localhost:3000/api/inscription",
+    API_LOGIN : "https://minijeux.nojii.fr/api/connexion", 
+    API_SIGNIN : "https://minijeux.nojii.fr/api/inscription", 
+    // API_LOGIN: "http://localhost:3000/api/connexion",
+    // API_SIGNIN: "http://localhost:3000/api/inscription",
 }
