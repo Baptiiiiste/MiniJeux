32fbcc338eba89f314a394a93177d31d4b2ab770
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2023-02-27 15:12:52 +0100

    A tester: "Fonction d'ajout des stats allumettes"

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // API_LOGIN : "https://minijeux.nojii.fr/api/login", 
-    // API_SIGNIN : "https://minijeux.nojii.fr/api/signin", 
-    API_LOGIN: "http://localhost:3000/api/login",
-    API_SIGNIN: "http://localhost:3000/api/signin",
+    API_LOGIN : "https://minijeux.nojii.fr/api/login", 
+    API_SIGNIN : "https://minijeux.nojii.fr/api/signin", 
+    // API_LOGIN: "http://localhost:3000/api/login",
+    // API_SIGNIN: "http://localhost:3000/api/signin",
 }
