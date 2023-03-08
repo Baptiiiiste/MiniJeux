a312e791813d70d10ae9ae40d7e4f1c1b790774a
Baptiiiiste <baptiste.Tab4@gmail.com>
2023-03-08 09:32:01 +0100

    vars

--- src/utils/models/User.js
+++ src/utils/models/User.js
@@ -1,4 +1,4 @@
-import { API_GET_ALLUMETTES_STATS, API_SET_ALLUMETTES_STATS, API_GET_BLACKJACK_STATS, API_SET_BLACKJACK_STATS } from "@/assets/variables";
+import { API_GET_MATCHES_STATS, API_SET_MATCHES_STATS, API_GET_BLACKJACK_STATS, API_SET_BLACKJACK_STATS } from "@/assets/variables";
 import useFetch from "../hooks/useFetch";
 
 export default class User {
@@ -18,18 +18,18 @@ export default class User {
 
     async addAllumettesStats(userWon) {
 
-        let resp = await useFetch.put(`${API_SET_ALLUMETTES_STATS}`, {totalGames: 1, user: this.pseudo, totalWins: userWon ? 1 : 0});
+        let resp = await useFetch.put(`${API_SET_MATCHES_STATS}`, {totalGames: 1, user: this.pseudo, totalWins: userWon ? 1 : 0});
         return resp;
 
     }
 
     async addBlackJackStats(userWon, diceThrown, blackjack, score, bust) {
-        let resp = await useFetch.put(`${API_SET_ALLUMETTES_STATS}`, {totalGames: 1, user: this.pseudo, totalWins: userWon ? 1 : 0, totalDiceThrownByUser: diceThrown, total21ByUser: blackjack, totalScore: score, totalBustByUser: bust});
+        let resp = await useFetch.put(`${API_SET_BLACKJACK_STATS}`, {totalGames: 1, user: this.pseudo, totalWins: userWon ? 1 : 0, totalDiceThrownByUser: diceThrown, total21ByUser: blackjack, totalScore: score, totalBustByUser: bust});
         return resp;
     }
 
     async getAllumettesStats() {
-        let resp = await useFetch.get(`${API_GET_ALLUMETTES_STATS}/${this.pseudo}`);
+        let resp = await useFetch.get(`${API_GET_MATCHES_STATS}/${this.pseudo}`);
         if(resp.success === false) return resp;
 
         const map = new Map();
